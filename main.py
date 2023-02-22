e1e39d77cf68b2c57ad094b0ee583682b5818c05
Daniel <66734096+Daniel-A3@users.noreply.github.com>
2023-02-22 21:57:29 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, os
 
 pygame.init()
 
-WIDTH, HEIGHT = 960, 540
+WIDTH, HEIGHT = 1280, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("OneOrZero")
 WHITE = (255, 255, 255)
@@ -10,6 +10,22 @@ WHITE = (255, 255, 255)
 FPS = 60
 
 AND_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "ANDGate.png"))
+AND_GATE = pygame.transform.scale(AND_GATE_IMAGE, (100,56))
+OR_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "ORGate.png"))
+OR_GATE = pygame.transform.scale(OR_GATE_IMAGE, (100,56))
+NOT_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "NOTGate.png"))
+NOT_GATE = pygame.transform.scale(NOT_GATE_IMAGE, (100,56))
+NAND_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "NANDGate.png"))
+NAND_GATE = pygame.transform.scale(NAND_GATE_IMAGE, (100,56))
+NOR_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "NORGate.png"))
+NOR_GATE = pygame.transform.scale(NOR_GATE_IMAGE, (100,56))
+XOR_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "XORGate.png"))
+XOR_GATE = pygame.transform.scale(XOR_GATE_IMAGE, (100,56))
+
+def drawWindow():
+    WIN.fill(WHITE)
+    WIN.blit(AND_GATE, (0,0))
+    pygame.display.update()
 
 def main():
 
@@ -19,8 +35,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        WIN.fill(WHITE)
-        pygame.display.update()
+        drawWindow()
 
     pygame.quit()
 
