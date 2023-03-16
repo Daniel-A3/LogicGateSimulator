79ff5d4c1f8ce25a913186371c95a825688c160c
Daniel <66734096+Daniel-A3@users.noreply.github.com>
2023-03-16 18:39:30 +0000

    fixed PNGs

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame, os
 
 pygame.init()
 
-WIDTH, HEIGHT = 1280, 720
+WIDTH, HEIGHT = 1332, 802
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("OneOrZero")
 WHITE = (255, 255, 255)
 
+BACKGROUND = pygame.image.load(os.path.join("Assets", "backgroundGrid.png"))
+
 FPS = 60
-# Loads the image and transforms it into an x:y ratio that is the same as its resolution.
-# This is so that all the icons are the same size.
+#  Ensures that all the icons are the same size.
 AND_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "ANDGate.png"))
 AND_GATE = pygame.transform.smoothscale(AND_GATE_IMAGE, (128, 64))
 OR_GATE_IMAGE = pygame.image.load(os.path.join("Assets", "ORGate.png"))
@@ -25,31 +26,26 @@ XOR_GATE = pygame.transform.smoothscale(XOR_GATE_IMAGE, (128, 64))
 
 def drawWindow():
     WIN.fill(WHITE)
-    WIN.blit(AND_GATE, (0,0))
-    WIN.blit(OR_GATE, (125,0))
-    WIN.blit(NOT_GATE, (250,0))
-    WIN.blit(NAND_GATE, (375,0))
-    WIN.blit(NOR_GATE, (500,0))
-    WIN.blit(XOR_GATE, (625,0))
+    WIN.blit(BACKGROUND, (250,0))
     pygame.display.update()
 
 def main():
-
+    # The main game loop
     run = True
     while run:
         for event in pygame.event.get():
+            # Quits the game if it is exited
             if event.type == pygame.QUIT:
                 run = False
-
         drawWindow()
 
     pygame.quit()
 
-    
+if __name__ == "__main__":
+    main()
+
+
 # Creates buttons for all logic gates
 #ANDGate = Button(pygame.image.load("andgate.png"))
 #ANDGate.rect.x = 50
 #ANDGate.rect.y = 20
-
-if __name__ == "__main__":
-    main()
